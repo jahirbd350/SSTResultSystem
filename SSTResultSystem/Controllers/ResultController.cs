1aaf566452db6bb51be253bbca421879f8a82674
Md Jahirul Islam <jahirbd350@gmail.com>
2022-03-06 16:00:58 +0600

    06 Mar 22 Update

--- SSTResultSystem/Controllers/ResultController.cs
+++ SSTResultSystem/Controllers/ResultController.cs
@@ -127,5 +127,117 @@ namespace SSTResultSystem.Controllers
             }
             return View("~/Views/Shared/Error.cshtml");
         }
+
+        [HttpPost]
+        public ActionResult ReportGeneration(FormCollection form)
+        {
+            string StudentId = form["StudentId"].ToString();
+            String ExamTerm = form["ExamTerm"].ToString();
+            string Semester = form["Semester"].ToString();
+            //Console.WriteLine(StudentId);
+            switch (Semester)
+            {
+                case "1st":
+                    try
+                    {
+                        result11 result = db.result11.Where(x => x.examterm == ExamTerm).FirstOrDefault();
+                        return RedirectToAction("index", "result11", new {  });
+                    }
+                    catch (Exception ex)
+                    {
+                        Session["err"] = ex.Message + "No Data Found!";
+                        return View("~/Views/Shared/Error.cshtml");
+                    }
+                    break;
+
+                case "2nd":
+                    try
+                    {
+                        result12 result = db.result12.Where(x => x.studentid == StudentId && x.examterm == ExamTerm).FirstOrDefault();
+                        return RedirectToAction("details", "result12", new { id = result.id });
+                    }
+                    catch (Exception ex)
+                    {
+                        return View("~/Views/Shared/Error.cshtml");
+                    }
+                    break;
+
+                case "3rd":
+                    try
+                    {
+                        result21 result = db.result21.Where(x => x.studentid == StudentId && x.examterm == ExamTerm).FirstOrDefault();
+                        return RedirectToAction("details", "result21", new { id = result.id });
+                    }
+                    catch (Exception ex)
+                    {
+                        return View("~/Views/Shared/Error.cshtml");
+                    }
+                    break;
+
+                case "4th":
+                    try
+                    {
+                        result22 result = db.result22.Where(x => x.studentid == StudentId && x.examterm == ExamTerm).FirstOrDefault();
+                        return RedirectToAction("details", "result22", new { id = result.id });
+                    }
+                    catch (Exception ex)
+                    {
+                        return View("~/Views/Shared/Error.cshtml");
+                    }
+                    break;
+
+                case "5th":
+                    try
+                    {
+                        result31 result = db.result31.Where(x => x.studentid == StudentId && x.examterm == ExamTerm).FirstOrDefault();
+                        return RedirectToAction("details", "result31", new { id = result.id });
+                    }
+                    catch (Exception ex)
+                    {
+                        return View("~/Views/Shared/Error.cshtml");
+                    }
+                    break;
+
+                case "6th":
+                    try
+                    {
+                        result32 result = db.result32.Where(x => x.studentid == StudentId && x.examterm == ExamTerm).FirstOrDefault();
+                        return RedirectToAction("details", "result32", new { id = result.id });
+                    }
+                    catch (Exception ex)
+                    {
+                        return View("~/Views/Shared/Error.cshtml");
+                    }
+                    break;
+
+                case "7th":
+                    try
+                    {
+                        result41 result = db.result41.Where(x => x.studentid == StudentId && x.examterm == ExamTerm).FirstOrDefault();
+                        return RedirectToAction("details", "result41", new { id = result.id });
+                    }
+                    catch (Exception ex)
+                    {
+                        return View("~/Views/Shared/Error.cshtml");
+                    }
+                    break;
+
+                case "8th":
+                    try
+                    {
+                        result42 result = db.result42.Where(x => x.studentid == StudentId && x.examterm == ExamTerm).FirstOrDefault();
+                        return RedirectToAction("details", "result42", new { id = result.id });
+                    }
+                    catch (Exception ex)
+                    {
+                        return View("~/Views/Shared/Error.cshtml");
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+            return View("~/Views/Shared/Error.cshtml");
+        }
     }
 }
